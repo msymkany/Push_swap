e60aac4ea6d5e6d1901ef8fa6a167af2c51cf96d
Maria Symkanych <msymkany@users.noreply.github.com>
2017-03-27 19:20:35 +0300

    added operations

---
+++ checker.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msymkany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/25 17:56:09 by msymkany          #+#    #+#             */
+/*   Updated: 2017/03/25 17:56:12 by msymkany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push.h"
+
+int		main(int ar, char **av)
+{
+	int 		*stack;
+	int			j;
+	t_stack		*list;
+	int 		fd;
+
+	j = 0;
+	if (ar == 1)
+		ft_usage(av[0]);
+	else
+	{
+		stack = (read_stack(ar, av, &j));
+		if (j == 0)
+		{
+			write(1, "NO sort is needed\n", 18); // test
+			while(--ar)
+			{
+				ft_putnbr(*stack++);
+				write(1, " ", 1);
+			}
+			write(1, "\n", 1);
+			exit(0);
+		}
+		else
+		{
+			list = push_to_stack(stack, ar);
+			write(1, "That's list\n", 12);
+			while (list)
+			{
+				ft_printf("%d\n", list->num);
+				ft_putchar('\n');
+				list = list->next;
+			}
+		}
+
+	}
+	return (0);
+}
---
+++ operations.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msymkany <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/27 15:23:49 by msymkany          #+#    #+#             */
+/*   Updated: 2017/03/27 15:23:52 by msymkany         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push.h"
+
+void	swap(t_stack **s)
+{
+	int 		n;
+
+	if (*s && (*s)->next)
+	{
+		n = (*s)->num;
+		(*s)->num = (*s)->next->num;
+		(*s)->next->num = n;
+	}
+}
+
+void	push(t_stack **frst, t_stack **scnd)
+{
+	t_stack		*st;
+
+	st = *frst;
+	if (*frst)
+	{
+		*frst = *frst->next;
+		st->next = *scnd;
+		*scnd = st;
+	}
+}
+
+void	rotate(t_stack **s)
+{
+	t_stack		*frst_el;
+	t_stack		*ptr;
+
+	frst_el = *s;
+	ptr = *s;
+	if (*s && (*s)->next)
+	{
+		*s = *s->next;
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = frst_el;
+		frst_el->next = NULL;
+	}
+}
+
+void	reverse(t_stack **s)
+{
+	t_stack		*last_el;
+	t_stack		*ptr;
+
+	ptr = *s;
+	if (*s && (*s)->next)
+	{
+		while (ptr->next->next)
+			ptr = ptr->next;
+		last_el = ptr->next;
+		ptr->next = NULL:
+		last_el->next = *s;
+		*s = last_el;
+	}
+}
+
+void	commands(char *com, t_stack *a)
+{
+	t_stack		*b;
+	int 		i;
+
+	i = 0;
+	if (com[])
+
+}
